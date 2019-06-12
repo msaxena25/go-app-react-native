bc3815a73a095fcd2b72099f85642632c85cbc1d
mohitsaxena <mohitsaxena2507@gmail.com>
2019-06-12 12:40:44 +0530

    Added  "react-native-paper" package and implement profile page. Working

--- goman/App.js
+++ goman/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ProviderProfile from "./components/providers/provider-profile";
+import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
+
+
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  }
+};
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    
+      <PaperProvider theme={theme}>
+        <ProviderProfile />
+      </PaperProvider>
+     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ goman/components/providers/provider-profile.js
@@ -0,0 +1,83 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { TextInput, FAB, Appbar, Text } from "react-native-paper";
+
+export default class ProviderProfile extends React.Component {
+  state = {
+    text: ""
+  };
+
+  _goBack = () => console.log("Went back");
+
+  _onSearch = () => console.log("Searching");
+
+  _onMore = () => console.log("Shown more");
+
+  render() {
+    return (
+      <View>
+        <Appbar.Header>
+          <Appbar.BackAction onPress={this._goBack} />
+          <Appbar.Content title="Title" subtitle="Subtitle" />
+          <Appbar.Action icon="search" onPress={this._onSearch} />
+          <Appbar.Action icon="more-vert" onPress={this._onMore} />
+        </Appbar.Header>
+
+        <View style={styles.container}>
+          <Text>Register</Text>
+          <TextInput
+            style={styles.inputField}
+            label="Name"
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+          <TextInput
+            style={styles.inputField}
+            label="Mobile No."
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+          <TextInput
+            style={styles.inputField}
+            label="City"
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+          <TextInput
+            style={styles.inputField}
+            label="State"
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+          <TextInput
+            style={styles.inputField}
+            label="Country"
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  parent: {},
+  container: {
+    marginTop: "20%",
+    padding: 10,
+    backgroundColor: "#ffffff",
+    position: "absolute",
+    justifyContent: "center",
+    width: "100%"
+  },
+  inputField: {
+    backgroundColor: "white"
+  },
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0
+  }
+});
